13aaf25b254d883c44c067f5c206cf40096f7e4a
Putcho1994 <b-boy-sprite-puwii@hotmail.com>
2025-04-12 18:05:15 +0200

    Clean up Cmake preset

--- src/kitsune_engine.cpp
+++ src/kitsune_engine.cpp
@@ -1,4 +1,3 @@
-#pragma once
 #include <kitsune_engine.hpp>
 
 KitsuneEngine::KitsuneEngine(KitsuneWindowing& windowing) : windowing_(windowing) {}
--- src/kitsune_windowing.cpp
+++ src/kitsune_windowing.cpp
@@ -1,4 +1,3 @@
-#pragma once
 #include <kitsune_windowing.hpp>
 
 
